4e9c32c92b0af44699b9498813f1d7f9011e3b47
Berke Kanber <berkekanber@gmail.com>
2025-06-09 19:48:06 +0300

    feat: add Breadcrumb component to dashboard layout for improved navigation

---
+++ src/app/feed/repositories/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Repositories() {
+  return (
+    <div>
+      <h1 className="text-lg font-medium">Repositories</h1>
+    </div>
+  );
+}
+
+export default Repositories;
