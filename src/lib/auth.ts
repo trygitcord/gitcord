5500f00c8eab0be3ee900a8bf75d47d7b734ba02
chefberke <berkekanber@gmail.com>
2025-08-28 03:37:33 +0300

    feat(auth): add httpOptions with timeout for GithubProvider

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -45,6 +45,9 @@ export const authOptions = {
     GithubProvider({
       clientId: process.env.GITHUB_ID as string,
       clientSecret: process.env.GITHUB_SECRET as string,
+      httpOptions: {
+        timeout: 10000,
+      },
       authorization: {
         params: {
           scope: "read:user user:email",
