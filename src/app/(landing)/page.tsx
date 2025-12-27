973722ba163641da48c616f0f7b57b6e2b3d1aad
chefberke <berkekanber@gmail.com>
2025-12-28 01:08:44 +0300

    feat: Add react-snowfall for enhanced visual effects on landing page

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,13 +1,17 @@
+"use client";
+
 import { HeroSection } from "@/components/landing/hero";
 import { Footer } from "@/components/landing/footer-section";
 import { Features } from "@/components/landing/features";
 import AppPreview from "@/components/landing/app-preview";
 import { Faq } from "@/components/landing/faq";
 import { MarqueeColumns } from "@/components/landing/marquee-columns";
+import SnowFall from "react-snowfall";
 
 export default function Home() {
   return (
     <div>
+      <SnowFall />
       <div>
         <HeroSection />
         <div className="max-w-6xl mx-auto pt-16 space-y-10">
